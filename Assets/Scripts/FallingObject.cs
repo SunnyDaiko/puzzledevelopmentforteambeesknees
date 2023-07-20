878320d6a0f73763076156d5076e522e7c617424
SunnyDaiko <98121273+SunnyDaiko@users.noreply.github.com>
2023-07-20 15:28:23 -0700

    Add files via upload

---
+++ Assets/Scripts/FallingObject.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingObject : MonoBehaviour
+{
+    public bool isFalling = false;
+    private float downSpeed = 0;
+    Vector3 checkpoint;
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.name == "player")
+            isFalling = true;
+        //Object disappears after 5 seconds
+        Destroy(gameObject, 5);
+        //the current position of the object
+        //where the object resets to
+        checkpoint = new Vector3(1.1f, -9.4f);
+    }
+
+    void Update()
+    {
+        if (isFalling)
+        {
+            //Acceleration of the object falling
+            //The higher the number the slower the object falls
+            downSpeed += Time.deltaTime / 29770;
+            //changing the position of the object falling(down)
+            transform.position = new Vector3(transform.position.x, transform.position.y - downSpeed, transform.position.z);
+        }
+
+        //whenever the object gets below -15.9 on the y axis
+        if (transform.position.y < -15.9)
+        {
+            //object resets to the original postion
+            transform.position = checkpoint;
+        }
+    }
+}
